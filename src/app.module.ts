b8b74eabe9401ebd00eb14aab514d0bd38a95d63
zigakk03 <zigakk03@gmail.com>
2024-03-29 13:25:28 +0100

    user fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./user/user.entity";
 
 @Module({
-  imports: [],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1237com1!',
+    database: 'reddit',
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  }), ],
   controllers: [AppController],
   providers: [AppService],
 })
